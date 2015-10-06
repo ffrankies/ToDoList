5b3ffe794d30f56cc5991cd3ccd89bc2abfc0cf3
Frank <wanyefrank@live.com>
2015-10-05 22:13:03 -0400

    GUI update
    Started building the GUI. Might have to change TaskList a bit to make it
    call the TaskWindow when adding a task. Or make TaskWindow return a task
    that I can add to TaskList.

--- Source/pack1/TaskGUI.java
+++ Source/pack1/TaskGUI.java
@@ -1,5 +1,45 @@
 package pack1;
 
-public class TaskGUI {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class TaskGUI extends JFrame implements ActionListener {
+
+	private JTable table;
+	private TaskList model;
+	private JScrollPane scrollPane;
+	private JButton add, close;
+	
+	public TaskGUI() {
+		model = new TaskList();
+		table = new JTable(model);
+		scrollPane = new JScrollPane(table);
+		
+		add = new JButton("Add");
+		add.addActionListener(this);
+		close = new JButton("Close");
+		close.addActionListener(this);
+		
+		add(scrollPane);
+		add(add);
+		add(close);
+		
+		setUndecorated(true);
+		setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton button = (JButton) e.getSource();
+		if(button == add) {
+			
+		}
+			
+	}
+	
 }
