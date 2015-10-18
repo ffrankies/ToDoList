f4faca712310d3eb451bf5f8becbd62b94b7ddaf
Frank <wanyefrank@live.com>
2015-10-18 02:44:47 -0400

    MyChooser, Repeatable Tasks
    - Continued Working on Repeatable Tasks
    - MyChooser can now choose between numbers
    - TaskWindow now switches JPanels based on repeatType value
    - Moved MyChooser actionListeners to TaskWindow, else they worked funny

--- Source/pack1/MyChooser.java
+++ Source/pack1/MyChooser.java
@@ -6,84 +6,106 @@ import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class MyChooser<E> extends JPanel implements ActionListener {
-	
+public class MyChooser extends JPanel { //implements ActionListener {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private ArrayList<E> data;
+	private ArrayList<String> data;
+	private ArrayList<Integer> nums;
 	private JLabel display;
 	private JButton left, right;
-	private Color trans = new Color(0,0,0,0);
-	private Color bckg = new Color(1,1,1,0.55f);
+	//private Color trans = new Color(0,0,0,0);
+	//private Color bckg = new Color(1,1,1,0.55f);
 	private int index;
 	//private E result;
-	
-	public MyChooser(ArrayList<E> data) {
+
+	public MyChooser(int min, int max) {
 		super();
-		this.data = data;
-		this.setOpaque(false);
-		
+		this.nums = new ArrayList<Integer>();
+		setOpaque(false);
+		for(int i = min; i <= max; i++) {
+			nums.add(i);
+		}
 		left = new JButton("<");
-		left.setContentAreaFilled(false);
-		left.setBorderPainted(false);
-		left.setSize(new Dimension(30,40));
-		left.setMargin(new Insets(0,0,0,0));
-		left.setFocusPainted(false);
-		//left.setOpaque(false);
-		//left.setFocusable(false);
-		left.addActionListener(this);
-		
+		setUpButton(left);
+
 		right = new JButton(">");
-		right.setContentAreaFilled(false);
-		//right.setFocusable(false);
-		right.setBorderPainted(false);
-		right.setSize(new Dimension(30,40));
-		right.setMargin(new Insets(0,0,0,0));
-		//button.setHorizontalTextPosition(SwingConstants.CENTER);
-		//button.setFocusPainted(false);
-		//button.setBorderPainted(false);
-		//right.setBackground(trans);
-		right.setFocusPainted(false);
-		right.addActionListener(this);
-		
-		display = new JLabel((String)data.get(0), JLabel.CENTER);
+		setUpButton(right);
+
+		display = new JLabel(""+min, JLabel.CENTER);
 		index = 0;
 		
 		setLayout(new BorderLayout());
 		add(BorderLayout.WEST, left);
 		add(BorderLayout.CENTER, display);
 		add(BorderLayout.EAST, right);
-//		add(left);
-//		add(display);
-//		add(right);
+		//		add(left);
+		//		add(display);
+		//		add(right);
 		setAlignmentY(SwingConstants.CENTER);
 		//setSize(new Dimension(200,40));
 		//setMargin(new Insets(0,0,0,0));
-		
+
 		setVisible(true);
 	}
-	
-	public void next() {
-		if(index < data.size()-1) {
-			index++;
-			setText(data.get(index));
-		}
-		else {
-			index = 0;
-			setText(data.get(index));
-		}
+
+	public MyChooser(ArrayList<String> data) {
+		super();
+		this.data = data;
+		this.setOpaque(false);
+
+		left = new JButton("<");
+		setUpButton(left);
+
+		right = new JButton(">");
+		setUpButton(right);
+
+		display = new JLabel((String)data.get(0), JLabel.CENTER);
+		index = 0;
+
+		setLayout(new BorderLayout());
+		add(BorderLayout.WEST, left);
+		add(BorderLayout.CENTER, display);
+		add(BorderLayout.EAST, right);
+		//		add(left);
+		//		add(display);
+		//		add(right);
+		setAlignmentY(SwingConstants.CENTER);
+		//setSize(new Dimension(200,40));
+		//setMargin(new Insets(0,0,0,0));
+
+		setVisible(true);
 	}
-	
+
+	public void next() {
+		if(data != null)
+			if(index < data.size()-1) {
+				index++;
+				setText(data.get(index));
+			}
+			else {
+				index = 0;
+				setText(data.get(index));
+			}
+		else
+			if(index < nums.size()-1) {
+				index++;
+				setText(nums.get(index));
+			}
+			else {
+				index = 0;
+				setText(nums.get(index));
+			}
+	}
+
 	public void prev() {
+		if(data != null)
 		if(index > 0) {
 			index--;
 			setText(data.get(index));
@@ -92,29 +114,64 @@ public class MyChooser<E> extends JPanel implements ActionListener {
 			index = data.size()-1;
 			setText(data.get(index));
 		}
+		else
+			if(index > 0) {
+				index--;
+				setText(nums.get(index));
+			}
+			else {
+				index = nums.size()-1;
+				setText(nums.get(index));
+			}
 	}
-	
-//	public void setTransparent() {
-//		this.setBorder(BorderFactory.createEmptyBorder());
-//		this.setBackground(trans);
+
+	//	public void setTransparent() {
+	//		this.setBorder(BorderFactory.createEmptyBorder());
+	//		this.setBackground(trans);
+	//	}
+
+	//@Override
+//	public void actionPerformed(ActionEvent e) {
+//		JButton button = (JButton)e.getSource();
+//		if(button == left)
+//			prev();
+//		if(button == right)
+//			next();
+//		repaint();
 //	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton button = (JButton)e.getSource();
-		if(button == left)
-			prev();
-		if(button == right)
-			next();
-		repaint();
-	}
-	
-	public void setText(E text) {
-		this.display.setText((String)text);
+	public void setText(String text) {
+		this.display.setText(text);
 		index = data.indexOf(text);
 	}
 	
-	public E getText() {
+	public void setText(int text) {
+		this.display.setText(""+text);
+		index = nums.indexOf(text);
+	}
+
+	public String getText() {
 		return data.get(index);
+	}
+	
+	public int getNum() {
+		return nums.get(index);
+	}
+
+	public void setUpButton(JButton button) {
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setSize(new Dimension(30,40));
+		button.setMargin(new Insets(0,0,0,0));
+		button.setFocusPainted(false);
+		//button.addActionListener(this);
+	}
+	
+	public JButton getLeft() {
+		return left;
+	}
+	
+	public JButton getRight() {
+		return right;
 	}
 }
--- Source/pack1/TaskWindow.java
+++ Source/pack1/TaskWindow.java
@@ -2,26 +2,19 @@ package pack1;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
+import java.util.*;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.event.*;
 import javax.swing.plaf.basic.BasicComboBoxUI;
+import javax.swing.text.AbstractDocument;
 
 public class TaskWindow extends JDialog implements ActionListener{
 
@@ -35,11 +28,9 @@ public class TaskWindow extends JDialog implements ActionListener{
 
 	private JTextField important;
 	
-	private JTextField description;
+	private JTextArea details;
 
 	private JButton okButton;
-
-	private JComboBox<String> repeatType;
 	
 	private JButton cancelButton;
 
@@ -47,20 +38,26 @@ public class TaskWindow extends JDialog implements ActionListener{
 	
 	private final Color bckg = new Color(1,1,1,0.55f);
 	private final Color trans = new Color(1,1,1,0f);
-	private final Color select = new Color(1,1,1,0.3f);
+	private final Color select = new Color(0,0,0,0.4f);
 
 	public static final boolean OK = true;
 	public static final boolean CANCEL = false;
 
 	private Task task;
 	
-	private JPanel noneP, numdayP, weekdayP;
+	private JPanel textBoxes, dLine, noneP, numdayP, weekdayP;
 
 	private SimpleDateFormat fmt = new SimpleDateFormat("MM/dd/yyyy");
 
 	protected int pX;
 
 	protected int pY;
+	
+	private MyChooser repeatType, dd, mm, yyyy, days;
+	
+	private JRadioButton[] buttons;
+	
+	private ArrayList<String> options;
 
 
 	public TaskWindow(JFrame paOccupy, Task task, TaskList list) {
@@ -79,9 +76,10 @@ public class TaskWindow extends JDialog implements ActionListener{
 		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
 		//Creates the transparent panel
-		JPanel textBoxes = new JPanel();
+		textBoxes = new JPanel();
 		textBoxes.setBackground(bckg);
-		textBoxes.setLayout(new GridLayout(6,1));
+		//textBoxes.setLayout(new GridLayout(6,1));
+		textBoxes.setLayout(new BoxLayout(textBoxes, BoxLayout.Y_AXIS));
 		textBoxes.setOpaque(false);
 		
 		//Instantiate text input boxes
@@ -90,6 +88,8 @@ public class TaskWindow extends JDialog implements ActionListener{
 		namePanel.add(new JLabel("Description:"));
 		namePanel.setOpaque(false);
 		name = new JTextField(task.getTaskName(),15);
+		((AbstractDocument) name.getDocument()).setDocumentFilter(
+				new DocumentSizeFilter(23));
 		setupTextField(name);
 		namePanel.add(name);
 		textBoxes.add(namePanel);
@@ -98,43 +98,47 @@ public class TaskWindow extends JDialog implements ActionListener{
 		JPanel descPanel = new JPanel();
 		descPanel.add(new JLabel("Details:"));
 		descPanel.setOpaque(false);
-		description = new JTextField(task.getDescription(),20);
-		setupTextField(description);
-		descPanel.add(description);
+		details = new JTextArea(task.getDescription(),4,20);
+		setupTextArea(details);
+		descPanel.add(details);
 		textBoxes.add(descPanel);
 
 		//Instantiates the repeat textfield
-		//textBoxes.add(new JLabel("Repeat:"));
-		ArrayList<String> options = new ArrayList();
+		options = new ArrayList<String>();
 		options.add("None");
 		options.add("Every x days");
 		options.add("Specific days");//{"None", "Every x days", "Specific Days"};
-		MyChooser<String> repeatType = new MyChooser<String>(options);
+		repeatType = new MyChooser(options);
+		repeatType.getLeft().addActionListener(this);
+		repeatType.getRight().addActionListener(this);
 		JPanel reptPanel = new JPanel();
 		reptPanel.add(new JLabel("Repeat:"));
 		reptPanel.add(repeatType);
-		//reptPanel.setAlignmentY(CENTER_ALIGNMENT);
 		reptPanel.setOpaque(false);
-//		Box box = new Box(BoxLayout.Y_AXIS);
-//		box.add(repeatType);
 		textBoxes.add(reptPanel);
+		
 		//Instantiates the deadline textfield
-		textBoxes.add(new JLabel("Deadline for the task:"));
-		//		GregorianCalendar cal = new GregorianCalendar(
-		//				TimeZone.getTimeZone("EST"));
-		//		Date date = cal.getTime();
-		Date date = task.getDate().getTime();
-		//choose = new JButton(fmt.format(date));
-		//dateChooser = new DateComboBox(fmt);
-		//choose.addActionListener(this);
-		deadline = new JTextField(fmt.format(date),30);
-		deadline.setBackground(trans);
-		deadline.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent me) {
-				repaint();
-			}
-		});
-		textBoxes.add(deadline);
+//		textBoxes.add(new JLabel("Deadline for the task:"));
+//		//		GregorianCalendar cal = new GregorianCalendar(
+//		//				TimeZone.getTimeZone("EST"));
+//		//		Date date = cal.getTime();
+//		Date date = task.getDate().getTime();
+//		//choose = new JButton(fmt.format(date));
+//		//dateChooser = new DateComboBox(fmt);
+//		//choose.addActionListener(this);
+//		deadline = new JTextField(fmt.format(date),30);
+//		deadline.setBackground(trans);
+//		deadline.addMouseListener(new MouseAdapter() {
+//			public void mousePressed(MouseEvent me) {
+//				repaint();
+//			}
+//		});
+//		textBoxes.add(deadline);
+		setupNoneP();
+		setupNumdayP();
+		setupWeekdayP();
+		dLine = noneP;
+		textBoxes.add(dLine);
 
 		JPanel impPanel = new JPanel();
 		impPanel.add(new JLabel("Is the task urgent?"));
@@ -193,8 +197,8 @@ public class TaskWindow extends JDialog implements ActionListener{
 
 		setUndecorated(true);
 		setBackground(bckg);
-		setSize(300,200);
-		//pack();
+		//setSize(300,240);
+		pack();
 		setVisible(true);
 	}
 
@@ -207,6 +211,29 @@ public class TaskWindow extends JDialog implements ActionListener{
 //			dateChooser = new DateComboBox(fmt);
 //			dateChooser.
 //		}
+		//Changes panel for choosing deadline depending on repeatType
+		if(e.getSource() == repeatType.getLeft())
+			repeatType.prev();
+		if(e.getSource() == repeatType.getRight())
+			repeatType.next();
+		if(e.getSource() == repeatType.getLeft() || 
+				e.getSource() == repeatType.getRight()) {
+			textBoxes.remove(dLine);
+			String rType = repeatType.getText();
+			if(rType.equals(options.get(0)))
+				dLine = noneP;
+			if(rType.equals(options.get(1)))
+				dLine = numdayP;
+			if(rType.equals(options.get(2)))
+				dLine = weekdayP;
+			textBoxes.add(dLine);
+			repaint();
+		}
+		if(e.getSource() == days.getLeft())
+			days.prev();
+		if(e.getSource() == days.getRight())
+			days.next();
+			
 		if(e.getSource() == okButton) {
 			closeStatus = OK;
 
@@ -225,7 +252,7 @@ public class TaskWindow extends JDialog implements ActionListener{
 				task.setDate(dueDate);
 				//date = fmt.parse(deadline.getText());
 				task.setTaskName(name.getText());
-				task.setDescription(description.getText());
+				task.setDescription(details.getText());
 				//task.setDate(deadline.getText());
 				if(important.getText().toLowerCase().equals("yes"))
 					task.setImportant(true);
@@ -245,6 +272,39 @@ public class TaskWindow extends JDialog implements ActionListener{
 		if(e.getSource() == cancelButton)
 			dispose();
 	}
+	
+	public void setupNoneP() {
+		noneP = new JPanel();
+		noneP.setOpaque(false);
+		//noneP.setLayout(new GridBagLayout());
+		noneP.add(new JLabel("Deadline:"));
+		//noneP.setVisible(true);
+	}
+	
+	public void setupNumdayP() {
+		numdayP = new JPanel();
+		numdayP.setOpaque(false);
+		numdayP.add(new JLabel("Task repeats every "));
+		days = new MyChooser(0,30);
+		days.getLeft().addActionListener(this);
+		days.getRight().addActionListener(this);
+		numdayP.add(days);
+		numdayP.add(new JLabel(" days."));
+	}
+	
+	public void setupWeekdayP() {
+		weekdayP = new JPanel();
+		weekdayP.setOpaque(false);
+		buttons = new JRadioButton[7];
+		String[] daysStrs = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+		for(int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JRadioButton(daysStrs[i]);
+			buttons[i].setBackground(trans);
+			buttons[i].setFocusPainted(false);
+			buttons[i].setContentAreaFilled(false);
+			weekdayP.add(buttons[i]);
+		}
+	}
 
 	public boolean getCloseStatus() {
 		return closeStatus;
@@ -252,8 +312,15 @@ public class TaskWindow extends JDialog implements ActionListener{
 	
 	public void setupTextField(JTextField field) {
 		field.setBackground(trans);
+		field.setSelectionColor(Color.WHITE);
 		field.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent me) {
+				repaint();
+			}
+			public void mouseReleased(MouseEvent me) {
+				repaint();
+			}
+			public void mouseDragged(MouseEvent me) {
 				repaint();
 			}
 		});
@@ -276,6 +343,72 @@ public class TaskWindow extends JDialog implements ActionListener{
 			
 		});
 		field.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				repaint();
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				repaint();
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				repaint();
+			}
+			
+		});
+	}
+	
+	public void setupTextArea(JTextArea area) {
+		area.setBackground(trans);
+		area.setSelectionColor(Color.WHITE);
+		area.setLineWrap(true);
+		area.setWrapStyleWord(true);
+		((AbstractDocument) area.getDocument()).setDocumentFilter(
+				new DocumentSizeFilter(130));
+		area.setBorder(new JTextField().getBorder());
+		//Is this actually necessary?
+		area.addCaretListener(new CaretListener() {
+
+			@Override
+			public void caretUpdate(CaretEvent arg0) {
+				repaint();
+			}
+			
+		});
+		area.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent me) {
+				repaint();
+			}
+			public void mouseReleased(MouseEvent me) {
+				repaint();
+			}
+			public void mouseDragged(MouseEvent me) {
+				repaint();
+			}
+		});
+		area.getDocument().addDocumentListener(new DocumentListener() {
+
+			@Override
+			public void changedUpdate(DocumentEvent arg0) {
+				repaint();
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent arg0) {
+				repaint();
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent arg0) {
+				repaint();
+			}
+			
+		});
+		area.addKeyListener(new KeyListener() {
 
 			@Override
 			public void keyPressed(KeyEvent arg0) {
